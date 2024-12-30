a3ff9e65de7724970d1044af3f006ddaec60b335
unknown <tmrafinha4@gmail.com>
2024-12-30 11:35:57 -0300

    security check added again

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,10 @@
-// import SecurityCheck from '../security/securityCheck';
+import SecurityCheck from '../security/securityCheck';
 import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
     return (
         <>
-            {/* <SecurityCheck /> */}
+            <SecurityCheck />
             <Outlet />
         </>
     );
